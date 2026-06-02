16d85416a316ecdb89f981801e26f716fce6efa0
S.Vignarathan <s.vignarathan@gmail.com>
2026-06-02 16:04:22 +0530

    Create variable&datatype1.js

---
+++ variables&datatypes1_28.04.2026/variable&datatype1.js
@@ -0,0 +1,30 @@
+ var x=10;   //old type
+console.log(x);
+
+var x=35.3;
+console.log(x);
+
+let y=30;
+console.log(y);
+
+// let y=50;
+// console.log(y);  //can't redeclare a let variable
+
+y=55;
+console.log(y);
+
+console.log(typeof y);
+console.log(typeof x);
+
+name='vikki'
+fname="vignarathan"
+lname=`sivalingam`   //from ES6 backtick __string template
+console.log(typeof name);
+console.log(typeof fname);
+console.log(typeof lname);
+
+console.log("my nick name is"+name); //string cancatenation
+console.log("my first name is"+fname);
+
+
+
