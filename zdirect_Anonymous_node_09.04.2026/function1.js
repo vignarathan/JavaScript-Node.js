50eb0f0ebf2b64e1eed2e83af2e293e655a3ef3f
S.Vignarathan <s.vignarathan@gmail.com>
2026-06-02 16:05:00 +0530

    Create function1.js

---
+++ zdirect_Anonymous_node_09.04.2026/function1.js
@@ -0,0 +1,10 @@
+myid();
+function myid(){
+    console.log("my id is 25044");
+}
+
+//myname();     //can't call a anonymous function before declaring it
+let myname=function (){
+
+    console.log("my name is vignarathan");
+}
