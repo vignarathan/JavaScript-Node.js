20aa4f57fef4083704e80e3597787ecfa71fc9a9
S.Vignarathan <s.vignarathan@gmail.com>
2026-06-03 17:00:27 +0530

    Delete function_callback.js

--- function_11.05.2026/function_callback.js
+++
@@ -1,3 +0,0 @@
-setTimeout(() => {
-    console.log("This message is displayed after 2 seconds.");
-}, 2000); //setTimeout is a built-in function that executes a function after a specified delay (in milliseconds)
